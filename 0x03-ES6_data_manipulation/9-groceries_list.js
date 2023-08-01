01c650867e1c41140cf1a8f44ca881366396d3b1
frank-Mwangi <kungofrank@gmail.com>
2023-08-01 18:23:05 +0300

    new Map from array

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -4,7 +4,7 @@ const groceriesList = () => {
     { key: 'Tomatoes', value: 10 },
     { key: 'Pasta', value: 1 },
     { key: 'Rice', value: 1 },
-    { key: 'Bananas', value: 5 },
+    { key: 'Banana', value: 5 },
   ];
   const newMap = new Map();
   list.forEach(({ key, value }) => newMap.set(key, value));
