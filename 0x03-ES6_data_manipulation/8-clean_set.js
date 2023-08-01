097c94d997d73780d2b9f4e10fecf86d0dd256d6
frank-Mwangi <kungofrank@gmail.com>
2023-08-01 19:26:55 +0300

    Clean up a set of strings

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,19 @@
+export default function cleanSet(set, startString) {
+  if (startString) {
+    return [...set]
+      .filter((element) => {
+        if (element) {
+          return element.startsWith(startString);
+        }
+        return "";
+      })
+      .map((element) => {
+        if (element) {
+          return element.replace(startString, "");
+        }
+        return "";
+      })
+      .join("-");
+  }
+  return "";
+}
