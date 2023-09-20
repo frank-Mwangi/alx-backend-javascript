800975635ea5a822433ce21cf103383a0bd30d47
frank-Mwangi <kungofrank@gmail.com>
2023-09-20 11:00:54 +0300

    Create a multiple endpoint HTTP server in Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const countStudents = require('./3-read_file_async');
+const express = require("express");
+const countStudents = require("./3-read_file_async");
 
-const port = '1245';
-const host = 'localhost';
+const port = "1245";
+const host = "localhost";
 
 const app = express();
-app.get('/', (req, res) => {
-  res.status(200).send('Hello Holberton School!');
+app.get("/", (req, res) => {
+  res.status(200).send("Hello Holberton School!");
 });
-app.get('/students', (req, res) => {
+app.get("/students", (req, res) => {
   countStudents(process.argv[2])
     .then((response) => {
-      res.status(200).send(`This is the list of our students\n ${response}`);
+      res.status(200).send(`This is the list of our students\n${response}`);
     })
     .catch((err) => {
       res.status(404).send(err.message);
